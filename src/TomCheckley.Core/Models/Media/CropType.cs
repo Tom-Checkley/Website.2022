2d5ad42da81c3d4f6b360641fad3bf821871220d
Tom Checkley <tomc@element78.co.uk>
2022-07-08 09:01:24 +0100

    Umbraco image cropper

---
+++ src/TomCheckley.Core/Models/Media/CropType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomCheckley.Core.Models.Media
+{
+    public class CropType
+    {
+        /// <summary>
+        /// The name of the crop in Umbraco
+        /// </summary>
+        public string Alias { get; set; }
+
+        /// <summary>
+        /// The base width of the crop.
+        /// </summary>
+        public float Width { get; set; }
+
+        /// <summary>
+        /// The base height of the crop.
+        /// </summary>
+        public float Height { get; set; }
+
+        /// <summary>
+        /// The ratio of the crop, calculated as height / width (eg, 1080 x 1920 = 0.5625)
+        /// </summary>
+        public float Ratio => Height / Width;
+    }
+}
---
+++ src/TomCheckley.Core/Models/Media/UmbracoMedia.cs
@@ -0,0 +1,16 @@
+﻿using TomCheckley.Core.Models.Base;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Extensions;
+
+namespace TomCheckley.Core.Models.Media
+{
+    public class UmbracoMedia : UmbracoContentBase, IMedia
+    {
+        public double Size => Exists ? _content.Value<double>("umbracoBytes") : 0;
+        public string AbsoluteUrl => Exists ? _content.Url(mode: UrlMode.Absolute) : string.Empty;
+
+        public UmbracoMedia(IPublishedContent content) : base(content)
+        {
+        }
+    }
+}
