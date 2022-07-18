bb948d8c4b62e185b7aff96f2623e87139b5d3d6
Tom Checkley <tomc@element78.co.uk>
2022-07-18 12:56:27 +0100

    CSP middleware

---
+++ src/TomCheckley.Core/Extensions/AppBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using TomCheckley.Core.Middleware;
+
+namespace TomCheckley.Core.Extensions
+{
+    public static class AppBuilderExtensions
+    {
+        public static IApplicationBuilder AddCustomHttpHeaders(this IApplicationBuilder application)
+        {
+            return application.UseMiddleware<CustomHttpHeadersMiddleware>();
+        }
+    }
+}
